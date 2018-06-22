b6be5ea83e2f133f414e745317f08ad618501a1d
AndreyBodnaruk <36486681+AndriyBod@users.noreply.github.com>
2018-06-22 08:23:50 +0300

    Add files via upload

--- synebo/src/Solution2.java
+++ synebo/src/Solution2.java
@@ -7,18 +7,21 @@ public class Solution2 {
         int b=1;
         int[] t= {-4, -8, -6, 9, 3, 13, 24, 30};
         int[] c=new int[t.length];
-
+        int s=t.length;
         for (int i = 0; i < t.length; i++) {
             a=a+t[i];
         }
         a=a/t.length;// Среднее Значение Температуры(СЗТ) с лужит точкой перехода от зимы к лету
         System.out.println("Temperature: ");
-        for (int i = 0; i < t.length; i++)
+
+        if(t.length < 1 ){
+            s=t.length+1;
+        for (int i = 0; i < s; i++)
         {
             System.out.print(t[i]+"\t");}
         System.out.println();
         c[0]=t[0];// так-как год начинается с зимы
-        for (int i = 1; i < t.length; i++)
+        for (int i = 1; i < s; i++)
         {
             if (t[i]<a | t[i]<=0){
                 c[b]=t[i];
@@ -31,5 +34,7 @@ public class Solution2 {
         }
         System.out.print("Days in winter:  ");
         System.out.println((b));
+        }else if (t.length>300000){
+            System.out.println("Array is too big");}
     }
 }
